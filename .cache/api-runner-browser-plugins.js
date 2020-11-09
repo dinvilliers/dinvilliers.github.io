f01d1ea29d4f361a36b4c50099bc861612c21b84
Wolfgang <wolfgangdslaurent@gmail.com>
2020-11-09 20:38:48 +0000

    Deploy

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
+      options: {"plugins":[],"trackingIds":["G-PLD47TB1DB"]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
