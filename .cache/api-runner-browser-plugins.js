05d818dffa07f89a7ad6d9c8ad4d3cfe4d350020
Wolfgang <wolfgangdslaurent@gmail.com>
2020-11-09 20:58:03 +0000

    Third commit

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/favicon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"de7eb4226c6395aa7dfa26b3fb4273a1"},
+    },{
       plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
       options: {"plugins":[],"trackingIds":["G-PLD47TB1DB"]},
     },{
