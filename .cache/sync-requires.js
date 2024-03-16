62604c80e20e9d7512ccfa4ce5e09272e4d3c7b2
Wolfgang <wolfgangdslaurent@gmail.com>
2024-03-16 22:01:21 +0000

    Update 2024

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/admin/Sites/dinvilliers.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/admin/Sites/dinvilliers.github.io/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/admin/Sites/dinvilliers.github.io/src/pages/about.js"))),
   "component---src-pages-alaska-js": hot(preferDefault(require("/Users/admin/Sites/dinvilliers.github.io/src/pages/alaska.js"))),
