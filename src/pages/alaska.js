80bc11b92505366b76dbdb83a2dfd175f577ab53
Wolfgang <wolfgangdslaurent@gmail.com>
2024-03-21 16:18:46 +0000

    Update Alaska page

--- src/pages/alaska.js
+++ src/pages/alaska.js
@@ -8,6 +8,7 @@ import alaska from "../images/alaska.jpg"
 import alaska1 from "../images/Alaska Wordpress Theme.png"
 import alaska2 from "../images/03_alaska.png"
 import alaska3 from "../images/02_alaska.png"
+import alaska4 from "../images/thelma sketch 2.png"
 import ScrollForMore from "../components/scrollForMore";
 
 const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
@@ -39,28 +40,45 @@ const Alaska = () => (
     </div>
     <div className="page-content">
 		    <div className="page-header">
-		    	<h2>Custom Wordpress Theme</h2>
+		    	<h2>Custom WordPress Theme</h2>
 		    	<Link className="project-redirect" to="http://alaska.dinvilliers.com">Visit Site</Link>
 		    </div>
 		    	<p className="page-paragraph">
-		    		A friend of my mine wanted a simple but stylish Wordpress theme for their blog. I decided
-		    		on a grid theme because the website is very picture heavy. Sadly, the
-		    		website is not up anymore. But the demo is still active.
+		    		A friend wanted a simple but stylish WordPress theme for their blog. It was 
+                    designed according to their specifications. We decided on a grid theme 
+                    because the blog is very picture heavy. Sadly, the website is not up anymore but 
+                    the demo is still active.
 		    	 </p>
 		    	 <div className="skills-table">
 		    	  <p className="about-skill">Tools</p>
 				    <ul className="about-skill-list skills-li">
 				        <li>HTML</li>
-				        <li>CSSS</li>
+				        <li>CSS</li>
 				        <li>PHP</li>
 				        <li>JavaScript</li>
+                        <li>UI/UX</li>
 				    </ul>
 				  </div>
 				  <img alt="Alaska theme 1" className="left-image" src={alaska1} />
-				  <div className="juneau-images">
-					  <img src={alaska2} alt="Alaska theme 2" />
-					  <img src={alaska3} alt="Alaska theme 2" />
+				  <div className="alaska-images">
+					  <img src={alaska2} alt="Alaska theme continued" />
+                      <br />
+                      <br />
+					  <img src={alaska3} alt="Alaska theme individual page" />
+                      <br />
+                      <br />
 				  </div>
+
+
+                 <p className="about-skill">Original mockup designed using Figma ↓</p>
+                 <br />
+                 <br />
+
+                 <div className="alaska-images">
+                      <img src={alaska4} alt="Alaska theme continued" />
+                      <br />
+                  </div>
+
 				  </div>
   </Layout>
 )
